12c978cdd344051c5195d46a65b92d5958da606a
Sandro Zbinden <sandrozbinden@gmail.com>
2016-09-21 07:15:30 +0100

    Refactor sort order buttons and create them dynamically. Now witch just adding a new enum a sort will be visible in the view.

--- src/NotePro/Models/TodoViewModels/TodoListViewModel.cs
+++ src/NotePro/Models/TodoViewModels/TodoListViewModel.cs
@@ -16,4 +16,18 @@ namespace NotePro.Models.TodoViewModels
     {
         FinishDate, CreatedDate, Priority
     }
+
+    public static class SortOrderExtensions
+    {
+        public static String GetName(this SortOrder sortOrder)
+        {
+            switch (sortOrder)
+            {
+                case SortOrder.CreatedDate: return "By created Date";
+                case SortOrder.FinishDate: return "By finished Date";
+                case SortOrder.Priority: return "By Priority";
+                default: throw new System.InvalidOperationException("Can't find sortOrder for enum: " + sortOrder); ;
+            }
+        }
+    }
 }
